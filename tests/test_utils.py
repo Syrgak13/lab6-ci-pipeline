5082f33a3c9d52b04dbe60b2ce5dc590dff838a5
Syrgak Tabyldiev <syrgaktabyldiev13@gmail.com>
2026-03-13 08:55:49 +0500

    fixed test_units.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,8 @@
-from src.utils import is_even, is_positive
-
+# Правильно по PEP8
 def test_is_even():
     assert is_even(2) is True
     assert is_even(3) is False
+
 
 def test_is_positive():
     assert is_positive(5) is True
